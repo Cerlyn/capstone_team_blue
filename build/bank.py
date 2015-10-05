4adb7b6047ccb45b1f01688849457b7b8b902307
Samuel Greenfeld <samuel@greenfeld.org>
2015-10-05 01:20:21 -0400

    Initial test TLS supporting webserver, SIGTERM handler added to bank.py

--- build/bank.py
+++ build/bank.py
@@ -5,8 +5,59 @@
 # Coursera Capstone
 # BIBIFI Fall 2015
 
-from OpenSSL import SSL, crypto
+import sys
+import socket
+import signal
+import tempfile
+import ssl
+from OpenSSL import crypto
 from common_utils import CommonUtils
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn, BaseServer
+
+TLStempfile = None
+
+
+def SIGTERMhandler(signum, frame):
+    try:
+        global TLStempfile
+        if TLStempfile is not None:
+            TLStempfile.close()  # Force deletion if possible
+    except Exception:
+        pass  # Program being requested to close
+    finally:
+        sys.exit(0)
+
+signal.signal(signal.SIGTERM, SIGTERMhandler)
+
+
+class TLSHandler(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+        self.timeout = 10
+
+    def do_GET(self):
+        self.connection.settimeout(10)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Hello World\n")
+
+
+class TLSHTTPServer(HTTPServer, ThreadingMixIn):
+        def __init__(self, address, port, tlscertificate, tlsprivkey):
+            BaseServer.__init__(self, (address, port), TLSHandler)
+            self.socket = ssl.SSLSocket(socket.socket(self.address_family,
+                                                      self.socket_type),
+                                        ssl_version=ssl.PROTOCOL_TLSv1_2,
+                                        certfile=TLStempfile.name,
+                                        server_side=True
+                                        )
+            self.server_bind()
+            self.server_activate()
+
+        def run(self):
+            self.serve_forever()
 
 
 class Bank:
@@ -69,6 +120,8 @@ class Bank:
         self._certauthoritynextserial = self._certauthoritynextserial + 1
 
     def setup_webcrypto(self):
+        global TLStempfile
+
         self._tlsprivatekey = crypto.PKey()
         self._tlsprivatekey.generate_key(crypto.TYPE_RSA, 2048)
 
@@ -80,6 +133,7 @@ class Bank:
         self._tlscert = crypto.X509()
         self._tlscert.set_subject(certreq.get_subject())
         self._tlscert.set_serial_number(self._certauthoritynextserial)
+        self._tlscert.set_issuer(self._certauthority.get_subject())
         self._tlscert.gmtime_adj_notBefore(0)
         self._tlscert.gmtime_adj_notAfter(86400*365*3)  # under CA's lifetime
         self._tlscert.set_pubkey(certreq.get_pubkey())
@@ -90,12 +144,37 @@ class Bank:
         self._tlscert.sign(self._certauthorityprivatekey, "sha256")
         self._certauthoritynextserial = self._certauthoritynextserial + 1
 
+        tlsfile = tempfile.NamedTemporaryFile()
+        tlsfile.file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM,
+                                                  self._tlsprivatekey))
+        tlsfile.file.write(crypto.dump_certificate(crypto.FILETYPE_PEM,
+                                                   self._tlscert))
+        tlsfile.file.write(crypto.dump_certificate(crypto.FILETYPE_PEM,
+                                                   self._certauthority))
+        tlsfile.file.flush()
+        TLStempfile = tlsfile
+        # Closing the file/program will destroy this temporary file
+
+    def start_webserver(self):
+        tlsServer = TLSHTTPServer(self._common_utils.get_ipaddress(),
+                                  self._common_utils.get_ipport(),
+                                  self._tlscert, self._tlsprivatekey)
+        sys.stdout.write("Ready\n")  # FIXME: should be 'created\n'
+        sys.stdout.flush()
+        tlsServer.run()
+
     def run(self):
         self.setup_ca()
         self.setup_webcrypto()
         self.setup_atmcrypto()
+        # FIXME: NEED TO WRITE ATM TICKET TO FILE
+        # Start mutlithreading
+        self.start_webserver()
 
 
 if __name__ == "__main__":
     mybank = Bank()
-    mybank.run()
+    try:
+        mybank.run()
+    except KeyboardInterrupt:
+        pass
