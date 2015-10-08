e1395ced9896c1dd57b8720a409ff70ebcc09cfb
Samuel Greenfeld <samuel@greenfeld.org>
2015-10-08 01:37:32 -0400

    Add ATM client I was working on (hoping someone else would)

--- build/atm.py
+++ build/atm.py
@@ -6,7 +6,27 @@
 # BIBIFI Fall 2015
 
 
+import os.path
+import signal
+import sys
+import tempfile
+import urllib3
 from common_utils import CommonUtils
+from _ssl import CERT_REQUIRED
+
+
+def SIGALRMhandler(signum, frame):
+    if tempcafile is not None:
+        try:
+            tempcafile.close()
+        except Exception:
+            pass
+        finally:
+            CommonUtils.atm_protocol_error_exit('TIMEOUT')
+
+signal.signal(signal.SIGALRM, SIGALRMhandler)
+
+tempcafile = None
 
 
 class ATM:
@@ -17,8 +37,111 @@ class ATM:
         self.error_exit = self._common_utils.error_exit
         self._common_utils.parse_opts()
 
+    def read_cardfile(self, card_filename):
+        cardhandle = file(card_filename, 'r')
+        card_data = cardhandle.readline().rstrip()
+        cardhandle.close()
+        return card_data
+
+    def write_cardfile(self, card_filename, card_data):
+        cardhandle = file(card_filename, 'w')
+        cardhandle.write(card_data)
+        cardhandle.close()
+
+    def get_remoteTLScert(self):
+        authfile_name = self._common_utils.get_authfilename()
+        authfile = file(authfile_name, 'r')
+        roughparts = authfile.read().split('-----\n')
+        authfile.close()
+        if len(roughparts) != 7:
+            raise Exception("Invalid bank auth file")
+        remotecert = "-----BEGIN CERTIFICATE-----\n" + roughparts[5] \
+            + "-----\n"
+        return remotecert
+
     def run(self):
-        pass
+        global tempcafile
+
+        utils = self._common_utils
+        transactiontype = utils.get_transactiontype()
+
+        card_filename = utils.get_cardfilename()
+        card_data = ""
+
+        if transactiontype != 'N':
+            if card_filename is not None and os.path.isfile(card_filename):
+                try:
+                    card_data = self.read_cardfile(card_filename)
+                except Exception:
+                    self.error_exit('Error loading ATM card')
+            else:
+                self.error_exit('Card file not found to load')
+        params = {}
+        if transactiontype == 'N':
+            params['action'] = 'new'
+            params['account'] = utils.get_account()
+            params['amount'] = utils.get_transactionamount()
+        elif transactiontype == 'G':
+            params['action'] = 'balance'
+            params['account'] = utils.get_account()
+            params['card'] = card_data
+        elif transactiontype == 'D':
+            params['action'] = 'deposit'
+            params['account'] = utils.get_account()
+            params['amount'] = utils.get_transactionamount()
+            params['card'] = card_data
+        elif transactiontype == 'W':
+            params['action'] = 'withdraw'
+            params['account'] = utils.get_account()
+            params['amount'] = utils.get_transactionamount()
+            params['card'] = card_data
+        else:
+            self.error_exit('Unknown transaction type - should never get here')
+
+        remotecert = self.get_remoteTLScert()
+        tempcafile = tempfile.NamedTemporaryFile(prefix='tmpATM')
+        tempcafile.write(remotecert)
+        tempcafile.flush()
+
+        signal.alarm(10)
+
+        try:
+            # We are using IPs, but certificates prefer names
+            # Override the SSL processing to look for our desired name
+            atmserverhostname = 'atmserver.bank.example.com'
+            httpsclient = urllib3.HTTPSConnectionPool(host=utils.get_ipaddress(),
+                                                      port=utils.get_ipport(),
+                                                      maxsize=1,
+                                                      ca_certs=tempcafile.name,
+                                                      cert_reqs=CERT_REQUIRED,
+                                                      retries=0,
+                                                      assert_hostname=atmserverhostname)
+            response = httpsclient.request('GET', '/atm.cgi', params)
+
+        except Exception as e:
+            raise e
+            self.error_exit('Communication failure')
+
+        signal.alarm(0)
+        tempcafile.close()
+        tempcafile = None
+
+        if response.status != 200:
+            self.error_exit('Remote error')
+
+        response_data = response.data
+        responselines = response_data.split('\n')
+
+        if transactiontype == 'N':
+            if len(responselines) == 1:
+                self.error_exit('No card info provided')
+            if os.path.exists(card_filename) == False:
+                self.write_cardfile(card_filename, responselines[1] + '\n')
+            else:
+                self.error_exit('Card file found-cannot overwrite')
+
+        sys.stdout.write(responselines[0] + '\n')
+        sys.stdout.flush()
 
 
 if __name__ == "__main__":
